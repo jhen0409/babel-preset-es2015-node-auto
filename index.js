11d19315010f7dde48681a407ef36fb8d9f01972
Jhen <developer@jhen.me>
2017-01-12 13:25:32 +0800

    Support node 7 (Use node6 preset)

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var versions = ['4', '5', '6'];
 var defaultPreset = 'babel-preset-es2015';
 var version = process.version[1];
 
+// node 7 convert to 6
+if (version === '7') {
+  version = '6'
+}
+
 module.exports = require(
   defaultPreset + (versions.indexOf(version) !== -1 ? '-node' + version : '')
 );
